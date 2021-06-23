04d18ccc03180bc77cdfe8c911af20319bcac4d8
Eric Rocha <ericrochaeric@gmail.com>
2021-06-23 01:28:11 -0300

    aula02

---
+++ src/pages/Home.tsx
@@ -0,0 +1,55 @@
+import { useHistory } from "react-router-dom"
+
+import { FiLogIn } from "react-icons/fi"
+
+import { Button } from '../components/Button'
+import { useAuth } from "../hooks/useAuth"
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo-dark.svg'
+import googleIconImg from '../assets/images/google-icon-dark.svg'
+
+
+import '../styles/auth.scss'
+
+export function Home() {
+  const history = useHistory();
+  const { user,signInWithGoogle } = useAuth();
+  
+  function handleCreateRoom() {
+    if(!user) {
+      signInWithGoogle()
+    }
+    history.push('/rooms/new')
+  }
+
+
+  return(
+    <div id='page-auth'>
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button className='create-room' onClick={handleCreateRoom}>
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input 
+              type="text" 
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">
+              <FiLogIn /> Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
